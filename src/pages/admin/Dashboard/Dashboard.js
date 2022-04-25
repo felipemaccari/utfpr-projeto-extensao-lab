4e2a8378c4bad19c78ca2bb7bab1b7653d936b50
Felipe Maccari <felipecesarmaccari@gmail.com>
2022-04-24 23:28:12 -0300

    refactor(pages): remove swapi call on dashboard

--- src/pages/admin/Dashboard/Dashboard.js
+++ src/pages/admin/Dashboard/Dashboard.js
@@ -1,41 +1,17 @@
-import { Flex, Text, Grid, GridItem, useToast } from "@chakra-ui/react";
-
-import LoadingBox from "components/LoadingBox";
-
-import { useQueryGetFilms } from "service/films";
+import { Flex, Text } from "@chakra-ui/react";
 
 const Dashboard = () => {
-  const toast = useToast();
-
-  const { data: films, isLoading } = useQueryGetFilms({
-    onSuccess: () => {
-      toast({
-        title: "sucesso no fetch!",
-        status: "success",
-        duration: 3000,
-        isClosable: true,
-      });
-    },
-  });
-
-  if (isLoading) {
-    return <LoadingBox />;
-  }
-
   return (
-    <Grid templateColumns="repeat(3, 1fr)" gap={6} px="15%" py="50px">
-      {films.map((film) => (
-        <GridItem w="100%" key={film.episode_id} mt="30px">
-          <Flex direction="column">
-            <Text textAlign="center" fontWeight="bold" mb="20px">
-              {film.title}
-            </Text>
-
-            <Text textAlign="justify">{film.opening_crawl}</Text>
-          </Flex>
-        </GridItem>
-      ))}
-    </Grid>
+    <Flex align="center" justify="center" height="calc(100vh - 80px)">
+      <Flex direction="column" align="center">
+        <Text fontWeight="bold" fontSize="20px">
+          Bem vindo ao
+        </Text>
+        <Text fontWeight="bold" fontSize="25px">
+          Sistema do Laboratório de Química
+        </Text>
+      </Flex>
+    </Flex>
   );
 };
 
