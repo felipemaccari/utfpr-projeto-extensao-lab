33872fc489d07d956d807cfff2da822b91752198
Felipe Maccari <felipecesarmaccari@gmail.com>
2022-05-01 00:15:10 -0300

    feat(service): add edit and delete methods to institutions

--- src/service/institutions.js
+++ src/service/institutions.js
@@ -19,3 +19,21 @@ export const useMutationCreateInstitution = (options = {}) =>
         .then((result) => result.data),
     options
   );
+
+export const useMutationEditInstitution = (options = {}) =>
+  useMutation(
+    (institutionData) =>
+      axios
+        .put(`${BASE_URL}/instituicao/${institutionData.id}`, institutionData)
+        .then((result) => result.data),
+    options
+  );
+
+export const useMutationDeleteInstitution = (options = {}) =>
+  useMutation(
+    ({ institutionId }) =>
+      axios
+        .delete(`${BASE_URL}/instituicao/${institutionId}`)
+        .then((result) => result.data),
+    options
+  );
